2c76916954e37a6189f055936ed487316b263fd3
MATT\Matthew <mpuent1@students.towson.edu>
2018-02-07 17:23:48 -0500

    initial commit

---
+++ src/main/java/com/code4life/namex/NamexApplication.java
@@ -0,0 +1,12 @@
+package com.code4life.namex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamexApplication.class, args);
+	}
+}
---
+++ src/test/java/com/code4life/namex/NamexApplicationTests.java
@@ -0,0 +1,16 @@
+package com.code4life.namex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NamexApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
