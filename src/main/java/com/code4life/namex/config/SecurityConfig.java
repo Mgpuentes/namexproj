5b10c3f086904d7c4ef21085e87b2872817328f7
MATT\Matthew <mpuent1@students.towson.edu>
2018-02-08 13:33:50 -0500

    Modified security config and home temp

--- src/main/java/com/code4life/namex/config/SecurityConfig.java
+++ src/main/java/com/code4life/namex/config/SecurityConfig.java
@@ -2,14 +2,21 @@ package com.code4life.namex.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment environment;
 
     private static final String[] PUBLIC_MATCHERS = {
             "/",
@@ -26,6 +33,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     protected void configure(HttpSecurity http) throws Exception {
+
+        List<String> activeProfiles = Arrays.asList(environment.getActiveProfiles());
+
+        activeProfiles.forEach(activeProfile -> {
+            if(activeProfile.equals("dev")) {
+
+                try {
+                    http
+                            .headers().frameOptions().disable();
+                    http
+                            .cors().disable().csrf().disable();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
 
         http
                 .authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll();
